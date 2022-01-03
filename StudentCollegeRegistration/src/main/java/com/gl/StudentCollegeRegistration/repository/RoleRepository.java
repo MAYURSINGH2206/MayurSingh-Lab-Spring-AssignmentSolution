542257d8c93cb170186b7cb2ab67cbc63f363240
MAYURSINGH2206 <71334517+MAYURSINGH2206@users.noreply.github.com>
2022-01-03 13:14:47 +0530

    Delete StudentCollegeRegistration directory

--- StudentCollegeRegistration/src/main/java/com/gl/StudentCollegeRegistration/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.gl.StudentCollegeRegistration.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.gl.StudentCollegeRegistration.entity.*;
-
-public interface RoleRepository extends  JpaRepository<Role, Integer> {
-
-}
